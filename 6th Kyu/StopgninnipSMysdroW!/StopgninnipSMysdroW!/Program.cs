69aea89d037c03900685d1a2a57f0472d81d2fe7
TomAndrew93 <T.A.Rose1993@gmail.com>
2023-01-17 12:55:20 +0000

    Not the most elegant of solutions to this challenge but got it working!
    https://www.codewars.com/kata/5264d2b162488dc400000001/csharp

---
+++ 6th Kyu/StopgninnipSMysdroW!/StopgninnipSMysdroW!/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine(Kata.SpinWords("You are almost to the last test"));
+Console.ReadLine();
+
+public class Kata
+{
+    public static string SpinWords(string sentence)
+    {
+        List<string> sentenceArr = new List<string>();
+
+        foreach (string word in sentence.Split(" "))
+        {
+            if (word.Length >= 5)
+            {
+                sentenceArr.Add(Reverse(word));   
+            } else
+            {
+                sentenceArr.Add(word);
+            }
+        }
+        return String.Join(" ", sentenceArr);
+    }
+
+    private static string Reverse(string word)
+    {
+        char[] chars = word.ToCharArray();
+        List<char> charsArr = new List<char>();
+        Array.Reverse(chars);
+        foreach (char c in chars)
+        {
+            charsArr.Add(c);
+        }
+
+       var reversedWord = string.Join("", charsArr);
+       return reversedWord;
+    }
+}
