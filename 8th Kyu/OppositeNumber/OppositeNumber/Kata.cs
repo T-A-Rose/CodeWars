16574596fc52c10da664cf39f36d8a9ea7160f0e
TomAndrew93 <T.A.Rose1993@gmail.com>
2023-01-17 11:02:03 +0000

    Refactoring of this challenge.

--- 8th Kyu/OppositeNumber/OppositeNumber/Kata.cs
+++ 8th Kyu/OppositeNumber/OppositeNumber/Kata.cs
@@ -3,20 +3,5 @@ Console.WriteLine("Hello, World!");
 
 public class Kata
 {
-    static public void main(String[] args)
-    {
-        Opposite(1);
-    }
-
-    public static int Opposite(int number)
-    {
-        var number1 = number.ToString();
-        if (number1.Contains("-"))
-        {
-            return Convert.ToInt32(number1.Remove(0, 1));
-        } else
-        {
-            return Convert.ToInt32(number1.Insert(0, "-"));
-        }
-    }
+    public static int Opposite(int number) => number* -1;
 }
