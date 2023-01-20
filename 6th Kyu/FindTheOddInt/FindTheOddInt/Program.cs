5819d7f9f2cc6817320758b5001b107866ac3ec6
TomAndrew93 <T.A.Rose1993@gmail.com>
2023-01-20 10:30:51 +0000

    Find the odd int kata complete, albeit over complicated.

---
+++ 6th Kyu/FindTheOddInt/FindTheOddInt/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class Kata
+{
+    public static int find_it(int[] seq)
+    {
+        List<int> oddList = seq.OfType<int>().ToList();
+        var oddCount = 0;
+        var numberGroups = oddList.GroupBy(i => i);
+        foreach (var x in numberGroups)
+        {
+            var curNum = x.Key;
+            var numCount = x.Count();
+            if (numCount > oddCount && numCount % 2 != 0)
+            {
+                oddCount = curNum;
+            }
+        }
+
+        return oddCount;
+    }
+}
+
