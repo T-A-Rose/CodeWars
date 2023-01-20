cf64c9bfb0a84fdeebcbb8517d6b9df658a52d65
TomAndrew93 <T.A.Rose1993@gmail.com>
2023-01-20 11:55:00 +0000

    Highest & Lowest kata complete.

---
+++ 7th Kyu/HighestAndLowest/HighestAndLowest/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public static class Kata
+{
+    public static string HighAndLow(string numbers)
+    {
+        int highest = int.MinValue, lowest = int.MaxValue;
+        foreach (var c in numbers.Split(" "))
+        {
+            if (Convert.ToInt32(c) >= highest) highest = Convert.ToInt32(c);
+            if (Convert.ToInt32(c) <= lowest) lowest = Convert.ToInt32(c);
+        }
+        return $"{highest} {lowest}";
+    }
+}
