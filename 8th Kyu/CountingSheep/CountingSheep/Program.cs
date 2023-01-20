e464d784fbcae4e4d4876ace4622cc327a50b34a
TomAndrew93 <T.A.Rose1993@gmail.com>
2023-01-20 12:17:56 +0000

    Minor refactor.

--- 8th Kyu/CountingSheep/CountingSheep/Program.cs
+++ 8th Kyu/CountingSheep/CountingSheep/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System.Linq;
 
-Console.WriteLine("Hello, World!");
+var sheeps = new bool[] { true, false, true };
+Console.WriteLine(Kata.CountSheeps(sheeps));
 
 public static class Kata
 {
     public static int CountSheeps(bool[] sheeps) => sheeps.Where(x => x.Equals(true)).Count();
-     
 }
