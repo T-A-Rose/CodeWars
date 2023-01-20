02eeb31b29bcbc343b782e846f895090ade3097b
TomAndrew93 <T.A.Rose1993@gmail.com>
2023-01-20 12:00:30 +0000

    Refactor of Highest & Lowest challenge using Linq

--- 7th Kyu/HighestAndLowest/HighestAndLowest/Program.cs
+++ 7th Kyu/HighestAndLowest/HighestAndLowest/Program.cs
@@ -5,12 +5,7 @@ public static class Kata
 {
     public static string HighAndLow(string numbers)
     {
-        int highest = int.MinValue, lowest = int.MaxValue;
-        foreach (var c in numbers.Split(" "))
-        {
-            if (Convert.ToInt32(c) >= highest) highest = Convert.ToInt32(c);
-            if (Convert.ToInt32(c) <= lowest) lowest = Convert.ToInt32(c);
-        }
-        return $"{highest} {lowest}";
+        var parsedArr = numbers.Split(" ").Select(int.Parse);
+        return $"{parsedArr.Max()} {parsedArr.Min()}";
     }
 }
