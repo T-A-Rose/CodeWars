e0dcf1a76600a064b189ce2f1fc24510e67e5aff
TomAndrew93 <T.A.Rose1993@gmail.com>
2023-01-24 09:00:19 +0000

    Keep Hydrated challenge complete (Already started this one apparently so thought I'd finish it)

---
+++ 8th Kyu/KeepHydrated/KeepHydrated/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine(Kata.Litres(1.5));
+
+public class Kata
+{
+    public static int Litres(double time) => (int)Math.Floor((time * 0.5));
+}
