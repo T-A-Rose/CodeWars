de3e59b3328d78c0f84520ee345f1792e4258f44
TomAndrew93 <T.A.Rose1993@gmail.com>
2023-07-12 14:45:48 +0100

    Not finished Mumbling Kyu

---
+++ 7th Kyu/Mumbling/Mumbling/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine(Accum("ZpglnRxqenU"));
+static String Accum(string s)
+{
+    char[] charArr = s.ToCharArray();
+    List<char> testList = new List<char>();
+    for (var i = 0; i < charArr.Length; i++)
+    {
+        if (!testList.Any())
+        {
+            testList.Add(charArr[i]);
+        } else if (testList[i - 1] != charArr[i])
+        {
+            for (int y = 0; y < i + 1; y++)
+            {
+                if (y == 0)
+                {
+                    testList.Add('-');
+                    testList.Add(char.ToUpper(charArr[i]));
+                }
+                else
+                {
+                    testList.Add(charArr[i]);    
+                }
+            }
+        }
+    }
+
+    var tempString = Path.Combine(testList.ToString());
+    return tempString;
+}
